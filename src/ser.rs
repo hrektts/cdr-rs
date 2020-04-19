5e506d5f913bbb9f2a4d6592e543cff64ad4e68f
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2020-04-19 23:38:09 +0200

    Remove size from array reference serialization

--- src/ser.rs
+++ src/ser.rs
@@ -172,9 +172,9 @@ where
     }
 
     fn serialize_bytes(self, v: &[u8]) -> Result<Self::Ok> {
-        let l = v.len();
-        self.write_usize_as_u32(l)?;
-        self.add_pos(l as u64);
+        // let l = v.len();
+        // self.write_usize_as_u32(l)?;
+        // self.add_pos(l as u64);
         self.writer.write_all(v).map_err(Into::into)
     }
 
