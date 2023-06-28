c2f392e8dc380ba9d50e8be258be0749a3c8a6ac
danieleades <33452915+danieleades@users.noreply.github.com>
2023-06-28 04:01:29 +0100

    Use `thiserror` to define 'Error' (#17)

--- src/error.rs
+++ src/error.rs
@@ -1,71 +1,49 @@
-use std::{self, fmt, io, str::Utf8Error};
+use std::{fmt, io, str::Utf8Error};
 
 /// Convenient wrapper around `std::Result`.
 pub type Result<T> = std::result::Result<T, Error>;
 
 /// The Error type.
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
+    #[error("{0}")]
     Message(String),
-    Io(io::Error),
+
+    #[error("{0}")]
+    Io(#[from] io::Error),
+
+    #[error("does not support the serde::Deserializer::deserialize_any method")]
     DeserializeAnyNotSupported,
+
+    #[error("expected 0 or 1, found {0}")]
     InvalidBoolEncoding(u8),
+
+    #[error("expected char of width 1, found {0}")]
     InvalidChar(char),
+
+    #[error("char is not valid UTF-8")]
     InvalidCharEncoding,
+
+    #[error("encapsulation is not valid")]
     InvalidEncapsulation,
-    InvalidUtf8Encoding(Utf8Error),
+
+    #[error("{0}")]
+    InvalidUtf8Encoding(#[source] Utf8Error),
+
+    #[error("each character must have a length of 1, given \"{0}\"")]
     InvalidString(String),
+
+    #[error("sequence is too long")]
     NumberOutOfRange,
+
+    #[error("sequences must have a knowable size ahead of time")]
     SequenceMustHaveLength,
+
+    #[error("the size limit has been reached")]
     SizeLimit,
+
+    #[error("unsupported type")]
     TypeNotSupported,
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        use Error::*;
-
-        match *self {
-            Message(ref msg) => fmt::Display::fmt(msg, f),
-            Io(ref err) => fmt::Display::fmt(err, f),
-            DeserializeAnyNotSupported => write!(
-                f,
-                "does not support the serde::Deserializer::deserialize_any method"
-            ),
-            InvalidBoolEncoding(v) => write!(f, "expected 0 or 1, found {}", v),
-            InvalidChar(v) => write!(f, "expected char of width 1, found {}", v),
-            InvalidCharEncoding => write!(f, "char is not valid UTF-8"),
-            InvalidEncapsulation => write!(f, "encapsulation is not valid"),
-            InvalidUtf8Encoding(ref err) => fmt::Display::fmt(err, f),
-            InvalidString(ref s) => {
-                write!(f, "each character must have a length of 1, given \"{}\"", s)
-            }
-            NumberOutOfRange => write!(f, "sequence is too long"),
-            SequenceMustHaveLength => {
-                write!(f, "sequences must have a knowable size ahead of time")
-            }
-            SizeLimit => write!(f, "the size limit has been reached"),
-            TypeNotSupported => write!(f, "unsupported type"),
-        }
-    }
-}
-
-impl std::error::Error for Error {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        use Error::*;
-
-        match *self {
-            Io(ref e) => Some(e),
-            InvalidUtf8Encoding(ref e) => Some(e),
-            _ => None,
-        }
-    }
-}
-
-impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Self {
-        Self::Io(err)
-    }
 }
 
 impl serde::de::Error for Error {
